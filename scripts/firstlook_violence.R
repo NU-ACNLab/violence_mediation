d4edb98835fcaa88635a14d113f12826e4e507b8
Ellyn Butler <ellynrbutler@gmail.com>
2021-12-09 12:02:10 -0600

    violence sanity checks

---
+++ scripts/firstlook_violence.R
@@ -0,0 +1,94 @@
+### This script creates some basic summary statistics and plots of the violence
+### data
+###
+### Ellyn Butler
+### December 9, 2021
+
+library('ggplot2')
+library('ggpubr')
+library('dplyr')
+library('ggcorrplot')
+
+viol_df <- read.csv('~/Documents/Northwestern/studies/mwmh/data/violence/violence_11-22-2021.csv')
+
+viol_df$v2.etv1 <- recode(viol_df$ETVv2_1, `1`=1, `2`=0)
+viol_df$v2.etv2 <- recode(viol_df$ETVv2_2, `1`=1, `2`=0)
+viol_df$v2.etv3 <- recode(viol_df$ETVv2_3, `1`=1, `2`=0)
+viol_df$v2.etv4 <- recode(viol_df$ETVv2_4, `1`=1, `2`=0)
+viol_df$v2.etv5 <- recode(viol_df$ETVv2_5, `1`=1, `2`=0)
+viol_df$v2.etv6 <- recode(viol_df$ETVv2_6, `1`=1, `2`=0)
+viol_df$v2.etv7 <- recode(viol_df$ETVv2_7, `1`=0, `2`=1)
+
+
+ever_df <- data.frame(Variable=rep(paste0('ETV', 1:7), 2),
+                      Violence=rep(c('Family Hurt or Killed', 'Friends Hurt or Killed',
+                        'Saw Attacked Knife', 'Saw Shot', 'Shoved/Kicked/Punched',
+                        'Attacked Knife', 'Shot At'), 2),
+                      Visit=c(rep('First', 7), rep('Second', 7)),
+                      N=c(nrow(viol_df[!is.na(viol_df$v1.etv1), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv2), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv3), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv4), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv5), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv6), ]),
+                        nrow(viol_df[!is.na(viol_df$v1.etv7), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv1), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv2), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv3), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv4), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv5), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv6), ]),
+                        nrow(viol_df[!is.na(viol_df$v2.etv7), ])),
+                      Proportion=c(sum(viol_df$v1.etv1, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv1), ]),
+                        sum(viol_df$v1.etv2, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv2), ]),
+                        sum(viol_df$v1.etv3, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv3), ]),
+                        sum(viol_df$v1.etv4, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv4), ]),
+                        sum(viol_df$v1.etv5, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv5), ]),
+                        sum(viol_df$v1.etv6, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv6), ]),
+                        sum(viol_df$v1.etv7, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v1.etv7), ]),
+                        sum(viol_df$v2.etv1, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv1), ]),
+                        sum(viol_df$v2.etv2, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv2), ]),
+                        sum(viol_df$v2.etv3, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv3), ]),
+                        sum(viol_df$v2.etv4, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv4), ]),
+                        sum(viol_df$v2.etv5, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv5), ]),
+                        sum(viol_df$v2.etv6, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv6), ]),
+                        sum(viol_df$v2.etv7, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$v2.etv7), ])
+                      ))
+
+prop_ever_plot <- ggplot(ever_df, aes(x=Violence, y=Proportion, fill=Violence)) +
+  theme_linedraw() + facet_grid(. ~ Visit) +
+  geom_bar(stat='identity', position='dodge') +
+  theme(legend.position='none', axis.title.x=element_blank(),
+		panel.spacing=unit(.1, 'lines'),
+    axis.text.x = element_text(angle=45, hjust=1, size=10)) +
+  scale_y_continuous(limits=c(0, 1), breaks=round(seq(0, 1, .1), digits=1)) +
+  ggtitle('Ever Experienced Violent Acts?')
+
+
+pdf('~/Documents/Northwestern/projects/violence_mediation/plots/prop_ever_violence.pdf')
+prop_ever_plot
+dev.off()
+
+# Correlation plots - dangerously low
+corr <- cor(viol_df[!is.na(viol_df$v1.etv1) & !is.na(viol_df$v2.etv2),
+  c(paste0('v1.etv', 1:7), paste0('v2.etv', 1:7))])
+corr_ever_plot <- ggcorrplot(corr)
+pdf('~/Documents/Northwestern/projects/violence_mediation/plots/corr_ever_violence.pdf')
+corr_ever_plot
+dev.off()
+
+# There shouldn't be any of these cases if they were in fact asked about "ever"
+bad_etv1 <- viol_df[viol_df$v1.etv1 == 1 & viol_df$v2.etv1 == 0 & !is.na(viol_df$v1.etv1) & !is.na(viol_df$v2.etv1),
+  c('v1.etv1', 'v2.etv1')]
+nrow(bad_etv1)
+
+
+# Filter dataframe for cleaned variables
+viol_df2 <- viol_df[, c('ID', paste0('v1.etv', 1:7), paste0('v2.etv', 1:7))]
+names(viol_df2)[names(viol_df2) == 'ID'] <- 'subid'
+
+
+
+
+
+#
