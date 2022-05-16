20969794f544fbf300ca1d28fe11af6e955941fb
Ellyn Butler <ellynrbutler@gmail.com>
2022-05-15 19:17:05 -0500

    adjust OHBM violence figure

--- scripts/OHBM_firstlook_violence.R
+++ scripts/OHBM_firstlook_violence.R
@@ -42,12 +42,18 @@ ever_df <- data.frame(Variable=paste0('ETV', 1:7),
                         sum(viol_df$etv7_ever, na.rm=TRUE)/nrow(viol_df[!is.na(viol_df$etv7_ever), ]))
                       )
 
+
+ever_df$Violence <- ordered(ever_df$Violence, c('Family Hurt or Killed', 'Friends Hurt or Killed',
+  'Saw Attacked Knife', 'Saw Shot',
+  'Attacked Knife', 'Shot At', 'Shoved Kicked Punched'))
+
 prop_ever_plot <- ggplot(ever_df, aes(x=Violence, y=Proportion, fill=Violence)) +
   theme_linedraw() + geom_bar(stat='identity', position='dodge') +
-  theme(legend.position='none', axis.title.x=element_blank(),
-		panel.spacing=unit(.1, 'lines'),
-    axis.text.x = element_text(angle=45, hjust=1, size=10)) +
-  scale_y_continuous(limits=c(0, .5), breaks=round(seq(0, .5, .1), digits=1))
+  theme(legend.position='none', axis.title.x=element_blank(), axis.title.y=element_text(size=7),
+		panel.spacing=unit(.1, 'lines'), axis.text.y=element_text(size=6),
+    axis.text.x = element_text(angle=45, hjust=1, size=6)) +
+  scale_y_continuous(limits=c(0, .5), breaks=round(seq(0, .5, .1), digits=1)) +
+  scale_fill_manual(values=c('deepskyblue3', 'steelblue1', 'springgreen3', 'palegreen1', 'pink1', 'violetred1', 'firebrick2'))
 
 # stats
 t.test(RCADS_sum ~ ever, data=viol_df)
@@ -55,6 +61,6 @@ t.test(RCADS_sum ~ ever, data=viol_df)
 sum(viol_df$ever)/nrow(viol_df)
 
 # export
-jpeg('/projects/b1108/projects/violence_mediation/plots/OHBM_prop_ever_violence.jpg', res=300, units='mm', width=100, height=100)
+jpeg('/projects/b1108/projects/violence_mediation/plots/OHBM_prop_ever_violence.jpg', res=300, units='mm', width=80, height=80)
 prop_ever_plot
 dev.off()
