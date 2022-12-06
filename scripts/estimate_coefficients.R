4af25350921792066e9d7a0d80a9cfa28a0b7b0f
Ellyn Butler <ellynrbutler@gmail.com>
2022-12-06 11:00:03 -0600

    more notes on coefficients

--- scripts/estimate_coefficients.R
+++ scripts/estimate_coefficients.R
@@ -19,37 +19,43 @@ m2.281_xm1_mod <- lm(region281 ~ ever + IL10, data=df)
 
 y_xm1m2_mod <- lm(RCADS_sum ~ ever + IL10 + region2 + region14 + region237 + region261 + region281, data=df)
 
-# Just through IL10
+##### Just through IL10
 betatheta <- m1_x_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['IL10']]
 # Minimally penalized:  0.1814, 0.0361
 # Unpenalized:          0.1814, 0.0892
+# Significance: -, -
 
-# Just through region2 (primary visual cortex)
+##### Just through region2 (primary visual cortex)
 zetapi_2 <- m2.2_xm1_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['region2']]
 # Minimally penalized:  -0.2019, -0.0186
 # Unpenalized:          -0.2501, 0.1288
 # ^ sign flipped... why?
 # A: Possibly because we aren't controlling for all the other M2 variables?
+# Significance: -/+, +
 
-# Just through region14 (inferior temporal gyrus)
+##### Just through region14 (inferior temporal gyrus)
 zetapi_14 <- m2.14_xm1_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['region14']]
 # Minimally penalized:  0.1920, -0.4548
 # Unpenalized:          0.1711, -0.1572
 # ^ Why are the unpenalized coefficients smaller in magnitude?
 # A: Possibly because we aren't controlling for all the other M1 variables?
+# Significance: -, +
 
-# Just through region237 (hippocampus)
+##### Just through region237 (hippocampus)
 zetapi_237 <- m2.237_xm1_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['region237']]
 # Minimally penalized:  0.2985, 0.4676
 # Unpenalized:          0.2935, 0.2009
 # ^ Why are the unpenalized coefficients smaller in magnitude?
+# Significance: +, +
 
-# Just through region261 (pallidum)
+##### Just through region261 (pallidum)
 zetapi_261 <- m2.261_xm1_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['region261']]
 # Minimally penalized:  -0.1922, -0.1503
 # Unpenalized:          -0.1876, -0.2267
+# Significance: -, +
 
-# Just through region281 (medial inferior cerebellum)
+##### Just through region281 (medial inferior cerebellum)
 zetapi_281 <- m2.281_xm1_mod$coefficients[['ever']]*y_xm1m2_mod$coefficients[['region281']]
 # Minimally penalized:  -0.2939, 0.1643
 # Unpenalized:          -0.2897, 0.1007
+# Significance: +, -
